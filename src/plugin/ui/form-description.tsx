c2afe3b15a05effc0fd7f0c8ac50bc2c9eece6d9
Ribbit <echo07200503@gmail.com>
2023-09-22 22:43:34 +0900

    🎨 プラグイン説明部分のデザインを微調整

--- src/plugin/ui/form-description.tsx
+++ src/plugin/ui/form-description.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 type Props = { last?: boolean };
 
 export const PluginFormDescription = styled.p<Props>`
-  font-size: 14px;
+  font-size: 13px;
   line-height: 1.5;
   color: #666;
   margin: 0;
--- src/plugin/ui/form-title.tsx
+++ src/plugin/ui/form-title.tsx
@@ -3,8 +3,8 @@ import styled from '@emotion/styled';
 type Props = {};
 
 export const PluginFormTitle = styled.h2<Props>`
-  font-weight: 500;
-  font-size: 20px;
+  font-weight: 600;
+  font-size: 18px;
   margin: 0;
   margin-bottom: 16px;
   border-bottom: 1px solid #ccc;
